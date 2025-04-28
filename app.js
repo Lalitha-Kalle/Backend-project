e9790363fbb1a356846697ec7e8b74b41abd6392
Lalitha-Kalle <kallelalitha04@gmail.com>
2025-04-28 18:04:05 +0530

    Added Mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express")
+const http = require("http")
 const app = express()
 const dotenv = require("dotenv")
+const connectDB = require("./databases/database")
 dotenv.config()
 
-app.listen(process.env.PORT, ()=> {
-  console.log(`Server is running at ${process.env.PORT}`)
+const server = http.createServer(app)
+
+connectDB()
+.then(()=> {
+  console.log("DB connected")
+  server.listen(process.env.PORT, () => {
+    console.log(
+      `App is running on port ${process.env.PORT} in ${
+          process.env.NODE_ENV || "development"
+        } mode`
+    )
+  })
+})
+.catch((e) => {
+  console.log("DB connection failed" + e.message);
 })
